9f841c627d1d33dadca44272537056567fb99cb1
Dmitry Ponyatov <dponyatov@gmail.com>
2025-12-18 12:35:53 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ README.md
@@ -0,0 +1,8 @@
+# `prectimer` 0.0.1
+## precision timer
+
+(c) Dmitry Ponyatov <<dponyatov@gmail.com>> 2025 MIT
+
+github: https://github.com/ponyatov/prectimer
+
+- precision timers on server-side Linux
---
+++ lib/legas.ml
@@ -0,0 +1,41 @@
+let app = Sys.getcwd () |> String.split_on_char '/' |> List.rev |> List.hd
+let title = "precision timer"
+let about = "\n- precision timers on server-side Linux\n"
+let author = "Dmitry Ponyatov"
+let email = "dponyatov@gmail.com"
+let year = 2025
+let version = "0.0.1"
+let license = "MIT"
+let github = "github: https://github.com/ponyatov/" ^ app
+
+let legas () =
+  mkd "lib" ();
+  Sys.command "git checkout --orphan `whoami`";
+  Sys.command "ln -fs ../em/legas legas";
+  Sys.command "cp ~/em/legas/legas.ml lib/legas.ml";
+  Sys.command "git add legas lib";
+  Sys.command "code lib/legas.ml"
+
+#use "legas/files.ml"
+files();;
+
+#use "legas/vscode.ml"
+vscode();;
+
+#use "legas/ocaml.ml"
+ocamldots();
+
+#use "legas/doc.ml"
+doc();
+
+#use "legas/mk.ml"
+mk();
+
+#use "legas/cpp.ml"
+cpp();
+
+#use "legas/cmake.ml"
+cmake();
+
+#use "legas/rust.ml"
+rust();
